9dd93cdd18430817fe84c962ea31031149d229e4
S1lentAFK <86916610+S1lentAFK@users.noreply.github.com>
2024-07-09 02:17:12 +0200

    Added multiple screens

---
+++ src/routes/util/aboutus/credits/+page.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+
+</script>
+
+<style>
+
+</style>
+
+<div class="bg-[#1C1C1E] min-h-screen flex flex-col p-4 text-gray-300">
+  <div class="flex items-center justify-center relative pt-4">
+    <a href="/util/aboutus" class="absolute left-4 bg-[#474747] mb-4 opacity-70 text-white text-2xl font-semibold rounded-full w-10 h-10 flex items-center justify-center">&lt;</a>
+    <h1 class="text-white text-2xl font-bold mb-4 ">Credits</h1>
+  </div>
+  <hr class="mb-4 border-t-1 border-[#474747]">
+  <div class="text-white">
+    <div class="flex items-center justify-center relative mb-2">Mentor and Coordinator</div>
+    <div class="flex items-center justify-center relative font-semibold mb-8">Predrag Brođanac</div>
+    <div class="flex items-center justify-center relative mb-2">Team Leader & Chief Software Engineer</div>
+    <div class="flex items-center justify-center relative font-semibold mb-8">Gabriel Janđel</div>
+    <div class="flex items-center justify-center relative mb-2">Chief Designer & Marketing Lead</div>
+    <div class="flex items-center justify-center relative font-semibold mb-8">Fran Glušac</div>
+    <div class="flex items-center justify-center relative mb-2">DevOps & Full-stack Software Engineer</div>
+    <div class="flex items-center justify-center relative font-semibold mb-8">Karlo Vizec</div>
+    <div class="flex items-center justify-center relative mb-2">Front-end Software Engineer</div>
+    <div class="flex items-center justify-center relative font-semibold mb-8">Sven Matković</div>
+    <div class="flex items-center justify-center relative mb-2">Front-end Software Engineer</div>
+    <div class="flex items-center justify-center relative font-semibold mb-8">Sven Matić</div>
+    <div class="flex items-center justify-center relative mb-2">Front-end Software Engineer</div>
+    <div class="flex items-center justify-center relative font-semibold mb-8">Fran Češljaš</div>
+    <div class="flex items-center justify-center relative mb-2">Front-end Software Engineer</div>
+    <div class="flex items-center justify-center relative font-semibold mb-8">Filip Sekelj</div>
+  </div>
+</div>
