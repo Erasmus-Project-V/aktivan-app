79971b9d2283c7457692dca6426fdea68280732f
S1lentAFK <86916610+S1lentAFK@users.noreply.github.com>
2024-07-13 19:49:00 +0200

    TimeFrameSelector

---
+++ src/lib/components/TimeFrameSelector.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    export let selected: string;
+    export let options: string[];
+
+    function selectOption(option: string): void {
+        selected = option;
+    }
+</script>
+
+<style>
+
+</style>
+
+<div class="bg-[#2C2C2E] rounded-b-3xl h-16">
+    <div class="flex justify-center text-white">
+        {#each options as option}
+            <div
+                class="cursor-pointer pt-4 pl-4 pr-4 pb-1 {selected === option ? 'font-bold text-[#5FA3C9] border-b-[#5FA3C9] border-b-2' : ''}"
+                on:click={() => selectOption(option)}
+            >
+                {option}
+            </div>
+        {/each}
+    </div>
+</div>
+
+<div class="flex-1 flex items-center justify-center pt-14">
+    {#if selected}
+        <span id="title" class="text-white text-2xl font-bold">{selected}</span>
+    {/if}
+</div>
---
+++ src/routes/stats/+page.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+    import TimeFrameSelector from "$lib/components/TimeFrameSelector.svelte";
+    import MenuButton from "$lib/components/MenuButton.svelte";
+
+    let options: string[] = ['This week', 'Chosen day', 'This month'];
+</script>
+
+<div>
+    <TimeFrameSelector options={options} selected={options[0]}></TimeFrameSelector>
+
+    <div class="bg-black text-white p-4">
+
+        <MenuButton href="/">
+            Running
+
+        </MenuButton>
+        <MenuButton href="/">
+            Cycling
+
+        </MenuButton>
+        <MenuButton href="/">
+            Walking
+
+        </MenuButton>
+        <MenuButton href="/">
+            Skating
+
+        </MenuButton>
+        <MenuButton href="/">
+            Hiking
+
+        </MenuButton>
+
+    </div>
+</div>
